72cf7c8113177344aa28ec364383baf9ca925ed8
Pierre Cazenave <titoucaz33@gmail.com>
2021-01-27 14:24:07 +0100

    ack_bouton
    return:
    1si appuyé une sec
    0 si juste pressé cours
    -1 si pas d'appuis

--- code/main.ino
+++ code/main.ino
@@ -1,13 +1,24 @@
 #include "M5Stack.h"
 
-
+#define int int32_t
 
 void setup()
 {
-M5.lcd.
+M5.begin(true, false, true);
+M5.Power.begin();
 }
 
 void loop()
 {
-sylvain t bo test
+	M5.update();
 }
+
+int ack_bouton(void)//bouton C est le bouton de droite
+{
+	if(M5.BtnC.pressedFor(1000))
+		return 1;//si le bouton est appuyé pendant 1 sec
+	else if(M5.BtnC.wasPressed())
+		return 0;//si un appuis court
+	else return -1;//si pas d'appuis
+}
+
