077acc8cb77768af9077f411c5ef88a8b52e3d24
Pierre Cazenave <titoucaz33@gmail.com>
2021-01-27 14:55:50 +0100

    ajout trt_bouton

--- code/main.ino
+++ code/main.ino
@@ -2,24 +2,25 @@
 
 #define int int32_t
 
-void setup()
-{
-M5.begin();
-M5.Power.begin();
 enum{
 	WHITE,
 	GREEN,
 	BLUE,
 	RED
-	}
+};
 int color = WHITE;
-M5.Lcd.fillScreen(WHITE);
 
+void setup()
+{
+M5.begin();
+M5.Power.begin();
+M5.Lcd.fillScreen(WHITE);
 }
 
 void loop()
 {
 	M5.update();
+	trt_bouton();
 }
 
 int ack_bouton(void)//bouton C est le bouton de droite
@@ -32,7 +33,11 @@ int ack_bouton(void)//bouton C est le bouton de droite
 }
 
 
-
+void trt_bouton(void)
+{
+	trt_couleur(ack_bouton());
+	act_couleur();
+}
 
 void act_couleur()//Rempli l'écran de la couleur séléctionnée
 {
@@ -42,17 +47,18 @@ switch(color)
 	case GREEN : M5.Lcd.fillScreen(GREEN); break;
 	case BLUE : M5.Lcd.fillScreen(BLUE); break;
 	case RED : M5.Lcd.fillScreen(RED); break;
+	default: break;
 }
 }
 
 void trt_couleur(int BP)//Passe d'une couleur a l'autre dans l'ordre
 {
 	if(BP==0){
-	color++;
-	if(color>RED){
-		color=WHITE;
+		color++;
+		if(color>RED){
+			color=WHITE;
 	}
-	else;
+		else;
 	}
 	else{color=WHITE;}
 }
