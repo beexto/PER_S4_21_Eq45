189036ea2d0e422a9f55f68557441dc1dd6e8f52
Pierre Cazenave <titoucaz33@gmail.com>
2021-02-03 14:58:41 +0100

    debut check phase 2

--- code/main_pas_legal/main_pas_legal.ino
+++ code/main_pas_legal/main_pas_legal.ino
@@ -5,8 +5,9 @@
 #define courbeSIZE 100
 Adafruit_NeoPixel pixels = Adafruit_NeoPixel(M5STACK_FIRE_NEO_NUM_LEDS, M5STACK_FIRE_NEO_DATA_PIN, NEO_GRB + NEO_KHZ800);
 int lum=0;
+int freq[6]={0,2000,1000,500,250,125};
 int tfreq=0;//periode à laquelle on doit clignoter
-int courbe[100];
+int courbe[courbeSIZE]={0,0,0,0,0,0,0,0,0,9,20,30,40,50,59,69,78,88,97,106,114,123,131,139,147,155,162,170,176,183,190,196,202,207,212,217,222,227,231,234,238,241,244,246,248,250,252,253,254,254,255,254,254,253,252,250,248,246,244,241,238,234,231,227,222,217,212,207,202,196,190,183,176,170,162,155,147,139,131,123,114,106,97,88,78,69,59,50,40,30,20,9,0,0,0,0,0,0,0,0};//400octets-->1%delamemoiredesprogrammes
 int icourbe=0;
 unsigned long tattente=0;
 unsigned long lastmillis=0;
@@ -63,12 +64,14 @@ void setup()
 M5.begin();//initialisation de l'objet M5stack -- celui qui contient les boutons
 M5.Power.begin();//allumage des peripheriques -- les leds sur les bords
 M5.Lcd.fillScreen(WHITE);//on remplis initialement en en blanc l'écran
+//initPhase2();
 }
 
 void loop()
 {
 	M5.update();//lecture de l'état des boutons
 	phaseUNO();//fonction de la premiere phase client
+	phase2();
 }
 
 bool pressed=false;//stocke si le bouton à été appuyé lors de la derniere boucle
@@ -112,63 +115,52 @@ void initPhase2()
 	}
 }
 
-void phaseDeux()//active toutes les fonctions de la phase 2
+void phase2()//active toutes les fonctions de la phase 2
 {
 	if(color.couleur==BLANC)
 	{
 		tattente=millis();
 		lastmillis=millis();
+		tfreq=0;
 	}
 	else
 	{
 		actionGraph();
 		changeFreq();
 	}
+	
+	afficherGraph();
 }
 
 void actionGraph()//fait clignoter les bargraph
 {
-	if(lastmillis==0)//si premier passage on initialise lastmillis au temps actuel
-		lastmillis=tattente;	
-		
 	if((millis()-lastmillis)>(tfreq/courbeSIZE))//si il est temps de passer a la case de la courbe d'apres
-	{	
+	{//le temps depuis le dernier mouvement>temps sur une case du tableau
 		icourbe++;
+		lastmillis=millis();
 		if(icourbe>courbeSIZE)
 		icourbe=0;
 	}
+}
+
+void afficherGraph()/*On change la couleur et la luminosité de chaque led*/
+{
 	
-	/*On change la couleur et la luminosité de chaque led*/
 	color.updateRGB();
 	for(int i=0;i<M5STACK_FIRE_NEO_NUM_LEDS;i++)
-	pixels.setPixelColor(i, color.r, color.g, color.b,courbe[icourbe]);
+	{
+		if(tfreq==0)
+			pixels.setPixelColor(i, color.r, color.g, color.b,0);
+		else
+			pixels.setPixelColor(i, color.r, color.g, color.b,courbe[icourbe]);
+	}
 	pixels.show();
 }
 
 void changeFreq()//change tfreq pour savoir à quelle frequenece clignoter
 {
-	
-	if(millis()-tattente<60000)
-	{
-	}
-	else if(millis()-tattente<120000)//2 mins
-	{
-		tfreq=2000;//0.5Hz
-	}
-	else if(millis()-tattente<180000)//3 mins
-	{
-		tfreq=1000;//1Hz
-	}
-	else if(millis()-tattente<240000)//4 mins
-	{
-		tfreq=500;//2Hz	
-	}
-	else if(millis()-tattente<300000)//5 mins
-	{
-		tfreq=250;//4Hz	
-	}
-	else if(millis()-tattente<360000)//6 mins
-	{
-		tfreq=125;//8Hz	
-	}
+	int a=(int)((millis()-tattente)/60000);
+	if(a>5)
+		a=5;
+	tfreq=freq[a];
 }
